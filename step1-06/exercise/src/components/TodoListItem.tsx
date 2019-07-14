df3f63054da9aced6cf42527d6ce63db36fc2f0c
Jinyang Li <mail@jinyangli.org>
2019-07-13 21:21:04 -0500

    Complete step1-06 exercise

--- step1-06/exercise/src/components/TodoListItem.tsx
+++ step1-06/exercise/src/components/TodoListItem.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
 
-export class TodoListItem extends React.Component<any, any> {
-  render() {
-    return (
-      <li className="todo">
-        <label>
-          <input type="checkbox" /> Todo 1
-        </label>
-      </li>
-    );
-  }
-}
+export const TodoListItem = (props: any) => {
+  let { label, completed } = props;
+  return (
+    <li className="todo">
+      <label>
+        <input type="checkbox" checked={completed} /> {label}
+      </label>
+    </li>
+  );
+};
